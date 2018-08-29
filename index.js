45b4bde1a89d2580ea01e39c5595018920c64907
Leon Gaban <leongaban@gmail.com>
2018-08-29 15:18:04 -0500

    added GET & POST

--- index.js
+++ index.js
@@ -12,6 +12,38 @@ app.get('/', (req, res) => {
   res.send('Welcome to Vidly');
 });
 
+/*
+ * { GET } all genres
+ */
+app.get('/api/genres', (req, res) => {
+  res.send(genres);
+});
+
+/*
+ * { POST } all genres
+ */
+app.post('/api/genres', (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+
+  genres.push(genre);
+  res.send(genre);
+});
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+    image: Joi.string().required()
+  };
+
+  return Joi.validate(genre, schema);
+}
+
 // PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
